4d38b7b85f0c362e06ec410d670746183c9f2c5b
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2024-07-18 13:16:23 -0300

    Preparação do front-end ejs para resultado da busca

--- src/controllers/buscarController.js
+++ src/controllers/buscarController.js
@@ -1,3 +1,11 @@
 exports.buscar = (req, res) => {
-    
+    const { cpf } = req.query;
+    console.log(cpf)
+
+
+    res.render('index', {
+        cpf: cpf,
+        nome: '', 
+        resultado: false,
+    });
 }
