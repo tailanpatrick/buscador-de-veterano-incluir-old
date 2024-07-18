5edfc873abb5374dbb9e01eac5174d7fc19eba10
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2024-07-18 16:44:23 -0300

    usando o model studant no controler para decidir qual resposta entregar para o usuário

--- src/controllers/buscarController.js
+++ src/controllers/buscarController.js
@@ -1,3 +1,36 @@
-exports.buscar = (req, res) => {
-    
+const Student = require('../models/student')
+
+exports.buscar = async (req, res) => {
+    const { cpf } = req.query;
+
+    try {
+
+        const student = await Student.findByCpf(cpf);
+
+        if (!student) {
+            return res.render('index', {
+                cpf: '',
+                name: '',
+                resultado: !!student
+            }); 
+          
+        }
+
+        return res.render('index', {
+            cpf: student.cpf,
+            name: student.name,
+            resultado: !!student
+        }); 
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Server error')
+    }
+
+
+    res.render('index', {
+        cpf: cpf,
+        name: '',
+        resultado: false,
+    });
 }
