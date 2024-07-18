5553a3c9db8f865f63a22eb2b879f47a1a2525a5
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2024-07-18 13:15:01 -0300

    removido auth e titulo de parametros renderizados para a index

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,8 +2,7 @@ exports.paginaInicial = (req, res, next) => {
     const auth = false;
 
     res.render('index', {
-        titulo: 'Buscador de Veterano Incluir',
-        auth: auth
+       
     });
 
 }
